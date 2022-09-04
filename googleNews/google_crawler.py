8cc44c06729288bff891342d4d1fd99248dd84e8
ohhappylife <sshon2@jhu.edu>
2022-09-04 17:44:06 -0400

    .

--- googleNews/google_crawler.py
+++ googleNews/google_crawler.py
@@ -3,36 +3,40 @@ from serpapi import GoogleSearch
 import pandas as pd
 import temp
 from transform_general import save_csv_file
+keyword = ""
 
-params = {
-    "api_key": temp.google(), # Please user your own API Key.
-    "engine": "google",
-    "q": "cola",     # keywords to be searched (Required)
-    "gl": "us",
-    "tbm": "nws"
-}
+def crawlData(keyword):
+    params = {
+        "api_key": temp.google(), # Please user your own API Key.
+        "engine": "google",
+        "q": keyword,     # keywords to be searched (Required)
+        "gl": "us",
+        "tbm": "nws"
+    }
 
-search = GoogleSearch(params)   # where data extraction happens on the backend
-results = search.get_dict()     # JSON - > Python dictionary
-result = results["news_results"]
+    search = GoogleSearch(params)   # where data extraction happens on the backend
+    results = search.get_dict()     # JSON - > Python dictionary
+    result = results["news_results"]
 
-df_temp = pd.DataFrame(result)
-df = pd.DataFrame(columns = ['Author', 'Published Date', 'Title', 'Text', 'Title_without_stopwords', 'Text_without_stopwords',
-       'Language', 'Site_url', 'Main_img_url', 'Type', 'Label', 'hasImage'])
-df['Author'] = df_temp['source']
-df['Published Date'] = df_temp['date']
-df['Title'] = df_temp['title']
-df['Text'] = df_temp['snippet']
-df['Title_without_stopwords'] = ''
-df['Text_without_stopwords'] = ''
-df['Language'] = 'English'
-df['Site_url'] = df_temp['link']
-df['Main_img_url'] = df_temp['thumbnail']
-df['Type']  = 'Unknown'
-df['Label'] = 'Unknown'
-df['hasImage'] = df_temp['thumbnail'].isnull() == False
+    df_temp = pd.DataFrame(result)
+    df = pd.DataFrame(columns = ['Author', 'Published Date', 'Title', 'Text', 'Title_without_stopwords', 'Text_without_stopwords',
+           'Language', 'Site_url', 'Main_img_url', 'Type', 'Label', 'hasImage'])
+    df['Author'] = df_temp['source']
+    df['Published Date'] = df_temp['date']
+    df['Title'] = df_temp['title']
+    df['Text'] = df_temp['snippet']
+    df['Title_without_stopwords'] = ''
+    df['Text_without_stopwords'] = ''
+    df['Language'] = 'English'
+    df['Site_url'] = df_temp['link']
+    df['Main_img_url'] = df_temp['thumbnail']
+    df['Type']  = 'Unknown'
+    df['Label'] = 'Unknown'
+    df['hasImage'] = df_temp['thumbnail'].isnull() == False
 
-#Show the data set
-today = date.today()
-fname = "google_" + str(today)
-save_csv_file.save_file(df, 'google_uncleared', fname)
+    #Show the data set
+    today = date.today()
+    fname = "google_" + str(today)
+    save_csv_file.save_file(df, 'google_uncleared', fname)
+
+    return df
--- googleNews/google_main.py
+++ googleNews/google_main.py
@@ -5,17 +5,17 @@ import pandas as pd
 import transform_general.remove_stop_words
 from googleNews import google_transform
 from transform_general import save_csv_file, create_Ngram
+from google_crawler import crawlData
 
-os.system('python google_crawler.py')
-today = date.today()
+def googleNews(keyword):
+  today = date.today()
 
-wdir = "google_uncleared/" + "google_" + str(today) + ".csv"
-df = pd.read_csv(wdir)
-df = google_transform.changeDateType(df)
-df = transform_general.remove_stop_words.remove_stopwords(df)
+  df = crawlData(keyword)
+  df = google_transform.changeDateType(df)
+  df = transform_general.remove_stop_words.remove_stopwords(df)
 
-today = date.today()
-fname = "cleaned_google" + str(today)
-save_csv_file.save_file(df, 'google_cleared',fname)
+  today = date.today()
+  fname = "cleaned_google" + str(today)
+  save_csv_file.save_file(df, 'google_cleared',fname)
 
-create_Ngram.Ngram(df, 'google_Ngram')
+  create_Ngram.Ngram(df, 'google_Ngram')
