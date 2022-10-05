599347d4e327a410030a9be92f11b408bc504cd8
ohhappylife <sshon2@jhu.edu>
2022-10-05 16:34:14 -0400

    Fix typo

--- logger.py
+++ logger.py
@@ -1,8 +1,8 @@
 import logging
 # Logs will be stored on ETLlog.txt file
-# Change the level to log with different levle (ERROR, CRITICAL, DEBUG ...)
+# Change the level to log with different level (ERROR, CRITICAL, DEBUG ...)
 logger = logging
 
-logger.basicConfig(filename='ETLlog.txt',
-                    format='%(levelname)s %(asctime)s :: %(message)s',
-                    level=logging.ERROR)
+logger.basicConfig(filename='ETLlog.txt', # file name to store logs
+                    format='%(levelname)s %(asctime)s :: %(message)s', # Format
+                    level=logging.ERROR) # log level ~ ERROR, CRITICAL, DEBUG ...
