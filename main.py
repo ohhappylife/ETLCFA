3eb6de6a188c8d2b83ad4ab003054df709b06a98
ohhappylife <sshon2@jhu.edu>
2022-10-28 14:48:16 -0400

    .

--- main.py
+++ main.py
@@ -11,8 +11,33 @@ from datetime import date
 from transform_general import resolve_encoding_issues
 import information
 from Analyze_General import extract_keyword, summraize_text
+from other_general import credential
+from other_general import load_csv_file
 
 logger.debug("start the process")
+
+cr = credential.credential
+ls = load_csv_file.readLinebyLine('credential.txt')
+
+cr.s3_bing_news_raw = ls[0]
+cr.s3_bing_news_cleaned = ls[1]
+cr.s3_google_news_raw = ls[2]
+cr.s3_google_news_cleaned = ls[3]
+cr.s3_news_api_raw = ls[4]
+cr.s3_news_api_cleaned = ls[5]
+cr.s3_news_catcher_raw = ls[6]
+cr.s3_news_catcher_cleaned = ls[7]
+cr.s3_ny_times_raw = ls[8]
+cr.s3_ny_times_cleaned = ls[9]
+cr.s3_poltifiact_raw = ls[10]
+cr.s3_Ngram = ls[11]
+cr.cr_google_key = ls[12]
+cr.cr_news_catcher_key = ls[13]
+cr.cr_news_api_key = ls[14]
+cr.cr_NYTimes_key = ls[15]
+cr.cr_Bing_key = ls[16]
+cr.cr_aws_key = ls[17]
+cr.cr_aws_secret = ls[18]
 
 try:
     keyword = sys.argv[1]  # To test at the Pycharm. Please change it into 0 after the testing.
