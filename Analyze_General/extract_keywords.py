1059855a8105e9eefb51c4c2a62d4e603b40b188
ohhappylife <sshon2@jhu.edu>
2022-11-23 09:15:21 -0500

    Bug Fix

--- Analyze_General/extract_keywords.py
+++ Analyze_General/extract_keywords.py
@@ -1,7 +1,6 @@
 from keybert import KeyBERT
 
 def summarize(df, col):
-  # No model is supplied; default model = cnn
   keybert = KeyBERT()
   keywords = keybert.extract_keywords(
     df[col],
@@ -10,5 +9,10 @@ def summarize(df, col):
     use_mmr=True,
     diversity=0.7
   )
-  df["keywords_" + col] = [k[0] for k in keywords]
+  i = 0
+  for keyword in keywords:
+    if len(keyword) ==0:
+      keywords[i] = "not available"
+    i = i + 1
+  df[col+"_keywords"] = [k[0] for k in keywords]
   return df
