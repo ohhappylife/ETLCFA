f5d12973fc10b01fdb8c69ba167ead69139b265e
ohhappylife <sshon2@jhu.edu>
2022-11-24 23:21:54 -0500

    .

--- store_to_s3.py
+++ store_to_s3.py
@@ -2,7 +2,7 @@ from config import credential_aws_key, credential_aws_secret
 def savetoBucket_csv(df, bn, fn):
   path = "s3://" + bn + "/" + fn
   df.to_csv(path,
-            sep='|||',
+            sep='|||', # delimeter
             storage_options={'key': credential_aws_key,
                              'secret': credential_aws_secret})
 
