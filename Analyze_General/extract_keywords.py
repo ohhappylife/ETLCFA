a42339f812492c99a49b1b9e5d51215f785cee4d
ohhappylife <sshon2@jhu.edu>
2022-12-04 01:37:10 -0500

    .

--- Analyze_General/extract_keywords.py
+++ Analyze_General/extract_keywords.py
@@ -1,15 +1,13 @@
-import nltk
-from keybert import KeyBERT
+from config import summarizer_numOfKeywords, summarizer_deduplication_threshold, summarizer_max_ngram_size
 import yake
 def summarize(df, col):
   df['keyword_'+col] = 'missing'
   language = 'en'
-  max_ngram_size = 3
-  deduplication_threshold = 0.9
-  numOfKeywords = 3
-  custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size,
-                                              dedupLim=deduplication_threshold,
-                                              top=numOfKeywords, features=None)
+  custom_kw_extractor = yake.KeywordExtractor(lan=language,
+                                              n=summarizer_max_ngram_size,
+                                              dedupLim=summarizer_deduplication_threshold,
+                                              top=summarizer_numOfKeywords,
+                                              features=None)
 
   extract_keywords = lambda x: [k[0] for k in custom_kw_extractor.extract_keywords(x)]
   df['keyword_'+col] = df[col].apply(extract_keywords)
